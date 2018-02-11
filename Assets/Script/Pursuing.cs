bfaede6b9793d2e270be438bf82397459f23bb04
Glen <gzaengle@outlook.com>
2018-02-10 21:11:07 -0500

    3 Enemy Movement Scripts Added

---
+++ Assets/Script/Pursuing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pursuing : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+
+    private Vector2 playerPos;
+    private Vector2 thisPos;
+
+    void Start()
+    {
+        playerPos = player.gameObject.transform.position;
+        thisPos = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (thisPos.x > playerPos.x)
+        {
+            Vector2 delta = playerPos - thisPos;
+            delta.Normalize();
+            this.gameObject.transform.Translate(delta * speed * Time.deltaTime);
+            playerPos = player.gameObject.transform.position;
+            thisPos = this.gameObject.transform.position;
+        }  
+        else
+        {
+            this.gameObject.transform.Translate(Vector2.left * speed * Time.deltaTime);
+            playerPos = player.gameObject.transform.position;
+            thisPos = this.gameObject.transform.position;
+        }
+    }
+}
